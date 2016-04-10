29a15a6772aef65dc14772651ab54397c656e1e6
Parth-Gandhi96 <gandhi.bapu96@gmail.com>
2016-04-10 11:50:19 +0530

    Update DerivedConcepts.java

--- DerivedConcepts.java
+++ DerivedConcepts.java
@@ -1,17 +1,16 @@
-import java.util.*;
 
+import java.util.*;
 public class DerivedConcepts {
-	private HashSet<String> _derivedconcepts = new HashSet< String>();
+    private HashSet<String> _derivedconcepts = new HashSet< String>();
 	
-	public void addDerived(String[] derived){
-		for (String s: derived){
-		//	System.out.println(s + "derived");
-			_derivedconcepts.add(s);
-		}			
-	}
+    public void addDerived(String[] derived){
+	for (String s: derived){
+            _derivedconcepts.add(s);
+	}			
+    }
 		
-	public HashSet<String> getDerivedConcepts(){
-		return _derivedconcepts;
-	}
+    public HashSet<String> getDerivedConcepts(){
+	return _derivedconcepts;
+    }
 	
 }
