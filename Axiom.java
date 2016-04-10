61ece9240444c07401d94b6aa6909167e832eadf
Parth-Gandhi96 <gandhi.bapu96@gmail.com>
2016-04-10 11:49:16 +0530

    Update Axiom.java

--- Axiom.java
+++ Axiom.java
@@ -1,23 +1,21 @@
+
+
 import java.util.HashSet;
-
 public class Axiom {
-
-	public static HashSet<String> _axioms = new HashSet<String>();
+    public static HashSet<String> _axioms = new HashSet<String>();	
+    public Axiom(){
 		
-	public Axiom(){
-		
+    }
+    public static String[] makeGraph(String s){
+	String[] ar = new String[2];
+	if(s.contains(">=")  || s.contains(">.") ){             // We are making Higher-->Lower Graph means if A is subset of B then Graph will be B-->A
+            ar[1] = s.split(Relation.getAxiomDelimiter())[0];
+            ar[0] = s.split(Relation.getAxiomDelimiter())[1];
+	}					
+	else{
+            ar = s.split(Relation.getAxiomDelimiter());
 	}
-	
-	public static String[] makeGraph(String s){
-		String[] ar = new String[2];
-		if(s.contains(">=")  || s.contains(">.") ){
-				ar[1] = s.split(Relation.getAxiomDelimiter())[0];
-				ar[0] = s.split(Relation.getAxiomDelimiter())[1];
-			}					
-		else{
-				ar = s.split(Relation.getAxiomDelimiter());
-			}
-		return ar;
-	}
+	return ar;
+    }
 		
 }
