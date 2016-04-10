fd39c043291b001380d71d11cef21629c6eff5bb
Parth-Gandhi96 <gandhi.bapu96@gmail.com>
2016-04-10 11:51:15 +0530

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,57 +1,51 @@
+
 import java.util.*;
 
 public class Parser {
+    private DerivedConcepts _derived = new DerivedConcepts();
+    private HashSet<String> _concepts = new HashSet< String>();
+    private HashSet<String> _derivedconcepts;
+    private PrimitiveConcepts primitive = new PrimitiveConcepts();
 	
-	private DerivedConcepts _derived = new DerivedConcepts();
-	private HashSet<String> _concepts = new HashSet< String>();
-//	private HashSet<String> _primitiveconcepts = new HashSet< String>();
-	private HashSet<String> _derivedconcepts;
-	private PrimitiveConcepts primitive = new PrimitiveConcepts();
-	
-	public void parse( String line ){
-		
-		Relation.makeRelation();
-		line = line.replaceAll("\\s","");
-		//System.out.println(line);
-		String lineDelimiter = Relation.getderivedelimiter();
-		if(line.contains("@")){
-			Axiom._axioms.add(line.split("@")[1]);
-			lineDelimiter = Relation.getAxiomDelimiter();
-			String[] parsedString = (line.split("@")[1]).split(lineDelimiter);
-			for(String s : parsedString){
-				_concepts.add(s);	
-			//	System.out.println(s);
-			}
-		}
-		else if(line.contains("#")){
-			Definition._definition.add(line.split("#")[1]);
-		}
-		else{
-		String[] parsedString = line.split(lineDelimiter);
-		String[] derived = new String[parsedString.length - 1];
-		System.arraycopy(parsedString, 0, derived, 0, parsedString.length-1);
-		_derived.addDerived(derived);
-		
-		lineDelimiter =  Relation.getprimitivedelimiter();
-		parsedString =  parsedString[parsedString.length - 1].split(lineDelimiter);
-	
-		for(String s : parsedString){
-			_concepts.add(s);	
-		}
-		}
-		
+    public void parse( String line ){	
+	Relation.makeRelation();                                                        // To Store all the Relation Accordingly in Relation class
+	line = line.replaceAll("\\s","");                                               // Removing all the Space (without meaning)
+	String lineDelimiter;
+	if(line.contains("@")){
+            Axiom._axioms.add(line.split("@")[1]);                                      //Removing @ From the Starting of the Axiom & Add that Axiom in the Set of AXIOMS in Axiom class
+            lineDelimiter = Relation.getAxiomDelimiter();                               //Get all Axiom-Delimiter seperated by OR in lineDelimiter
+            String[] parsedString = (line.split("@")[1]).split(lineDelimiter);          //Splittting Axiom using Axiom-Delimiter
+            for(String s : parsedString){
+		_concepts.add(s);                                                       //Adding all the Splitted String (May be Primitive Concepts OR Derived Concept) in to Concepts (HashSet)
+            }
 	}
-	public void separateConcepts(){
-		_derivedconcepts = _derived.getDerivedConcepts();
-		for(String s : _concepts){
-			System.out.println(s);
-			if(!_derivedconcepts.contains(s)){
-				System.out.println(s + "primitive");
-				primitive.addPrimitive(s);
-				
-			}				
-		}
-		primitive.createGraph();
-		primitive.print();
+	else if(line.contains("#")){
+            Definition._definition.add(line.split("#")[1]);                             //Removing # From the Starting of the Definition & Add that Definition in the Set of Definition in Definition class
 	}
+	else{
+            lineDelimiter = Relation.getderivedelimiter();                              //Get all Derived-Delimiter seperated by OR in lineDelimiter
+            String[] parsedString = line.split(lineDelimiter);                          //Splittting the line using Derived-Delimiter
+            String[] derived = new String[parsedString.length - 1];
+            System.arraycopy(parsedString, 0, derived, 0, parsedString.length-1);
+            _derived.addDerived(derived);                                               //Adding the Derived Concept - Which are in the left of Derived-Delimiter
+		
+            lineDelimiter =  Relation.getprimitivedelimiter();                          //Get all Primitive-Delimiter seperated by OR in lineDelimiter
+            parsedString =  parsedString[parsedString.length - 1].split(lineDelimiter); // Splitting the Right part of line (After spltiing using Derived-Delimiter) using primitive-Delimiter
+	
+            for(String s : parsedString){
+		_concepts.add(s);                                                       //Adding Those (May be Primitive Concepts OR Derived Concept) in to concept (HashSet)
+            }
+	}		
+    }
+    public void separateConcepts(){
+	_derivedconcepts = _derived.getDerivedConcepts();                               
+	for(String s : _concepts){                                                      //Checking that all Concepts From the concept(HashSet)
+            if(!_derivedconcepts.contains(s)){                                          //NOT Derived Concepts are Primitive Concepts
+		primitive.addPrimitive(s);                                              // Adding those primitive Concepts in to primitive concepts (Hashset) of class primitive concept
+            }				
+	}
+	primitive.createGraph();                                                        
+	primitive.print();
+    }
 }
+
